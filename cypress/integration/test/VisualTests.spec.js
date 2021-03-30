5d1f863b48909dda3b7531b4e263e0ac09872fdc
Ehab Shaaban <ehab.shaaban@kites-software.com>
2021-03-30 12:58:15 +0200

    Add feature integration

--- cypress/integration/test/VisualTests.spec.js
+++ cypress/integration/test/VisualTests.spec.js
@@ -4,12 +4,14 @@ import LoginPage from '../page/LoginPage'
 import Utils from '../utils/Utils'
 
 describe('', function() {
-
+    beforeEach(function(){
+        cy.fixture('login_credentials').as('loginCredentials')
+    })
     it('', function(){
         const loginPage = new LoginPage()
         const logout = new Utils()
         loginPage.visit()
-        loginPage.login("test@example.com", "password")
+        loginPage.login(this.loginCredentials.dev.username, this.loginCredentials.dev.passwd)
         logout.logout()
         
     })
