38dccc7e5ab22f14d9ffa9eb4a0616b7cf6cbfdd
Ehab Shaaban <ehab.shaaban@kites-software.com>
2021-04-25 17:05:12 +0200

    Update create-inventory-general-info.js

--- cypress/integration/test/helpers/create-inventorty/create-inventory-general-info.js
+++ cypress/integration/test/helpers/create-inventorty/create-inventory-general-info.js
@@ -35,14 +35,6 @@ module.exports = ({
 
     createInv.listingMileageId().type(listingMileage)
 
-    createInv.exteriorColorDiv().click()
-
-    createInv.interiorColorColorDiv().click()
-
-    for(var i=0; i<=5;i++){
-        createInv.featuresDiv(i).click()
-    }
-
     createInv.cityFuelEcoId().type(cityFuelEco)
 
     createInv.highwayFuelEcoId().type(highwayFuelEco)
