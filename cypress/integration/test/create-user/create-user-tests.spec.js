ce8c22325619b5f591533ac37c459c1e517e9abf
Ehab Shaaban <ehab.shaaban@kites-software.com>
2021-05-10 14:26:08 +0200

    Update create user scripts with assert after creation

--- cypress/integration/test/create-user/create-user-tests.spec.js
+++ cypress/integration/test/create-user/create-user-tests.spec.js
@@ -3,24 +3,34 @@
 import {adaptToReduxPersist} from '../../utils/redux'
 import UserPage from '../../page/user-page'
 import createUserCore from '../helpers/create-user/user-core'
+import assetUser from '../helpers/create-user/user-assert'
 import faker from 'faker'
 
 const userPage = new UserPage()
 
-let userData
+let userCardInfo
 let loginCredentials
 
 describe('create users suite', function() {
 
     before(function() {
 
-        cy.fixture('create_user_data').then(function(usr) {userData = usr})
+        cy.fixture('create_user_data').then(function(usr) {userCardInfo = usr})
 
         cy.login(Cypress.env("USERNAME"), Cypress.env("PASSWD")).then(function(creds) {loginCredentials = creds})
 
     })
 
     beforeEach(function(){
+
+        let usrInfo = {
+            firstName     : faker.name.firstName(),
+            lastName      : faker.name.lastName(),
+            email         : faker.internet.email(),
+            contactNumber : faker.phone.phoneNumber(),
+            salesNumber   : faker.finance.account(),
+            title         : faker.name.title()
+        }
 
         cy.visit('./users', {
             onBeforeLoad (win) {
@@ -30,21 +40,23 @@ describe('create users suite', function() {
 
         userPage.addUserBtn().should('have.text', 'Add User')
 
-        createUserCore({userEmail:faker.internet.email()})
+        createUserCore({usrInfo : usrInfo})
+
+        assetUser({usrInfo:usrInfo})
     })
 
     it('create new user in sales department with multiple roles', function(){
         
         let roles =[
-            userData.role.marketing,
-            userData.role.sales,
-            userData.role.finance,
-            userData.role.accounting,
-            userData.role.services,
-            userData.role.admin
+            userCardInfo.role.marketing,
+            userCardInfo.role.sales,
+            userCardInfo.role.finance,
+            userCardInfo.role.accounting,
+            userCardInfo.role.services,
+            userCardInfo.role.admin
         ]
 
-        userPage.departmentID().type(userData.department.sales).type('{enter}')
+        userPage.departmentID().type(userCardInfo.department.sales).type('{enter}')
         userPage.selectMultipleRoles(...roles)
 
     })
@@ -52,62 +64,62 @@ describe('create users suite', function() {
     it('create new user in sales department with multiple roles', function(){
         
         let roles =[
-            userData.role.sales,
-            userData.role.accounting,
-            userData.role.admin
+            userCardInfo.role.sales,
+            userCardInfo.role.accounting,
+            userCardInfo.role.admin
         ]
 
-        userPage.departmentID().type(userData.department.sales).type('{enter}')
+        userPage.departmentID().type(userCardInfo.department.sales).type('{enter}')
         userPage.selectMultipleRoles(...roles)
 
     })
 
     it('create new user in IT department with services role', function(){
-        userPage.slectDepartmentAndRole(userData.department.it, userData.role.services)
+        userPage.slectDepartmentAndRole(userCardInfo.department.it, userCardInfo.role.services)
     })
     
     it('create new user in accountants department with accounting role', function(){
-        userPage.slectDepartmentAndRole(userData.department.accountants, userData.role.accounting)
+        userPage.slectDepartmentAndRole(userCardInfo.department.accountants, userCardInfo.role.accounting)
     })
 
     it('create new user in marketing department with marketing role', function(){
-        userPage.slectDepartmentAndRole(userData.department.marketing, userData.role.marketing)
+        userPage.slectDepartmentAndRole(userCardInfo.department.marketing, userCardInfo.role.marketing)
     })
 
     it('create new user in service department with services role', function(){
-        userPage.slectDepartmentAndRole(userData.department.service, userData.role.services)
+        userPage.slectDepartmentAndRole(userCardInfo.department.service, userCardInfo.role.services)
     })
 
     it('create new user in sales department with sales role', function(){
-        userPage.slectDepartmentAndRole(userData.department.sales, userData.role.sales)
+        userPage.slectDepartmentAndRole(userCardInfo.department.sales, userCardInfo.role.sales)
     })
 
     it('create new user in IT department with admin role', function(){
-        userPage.slectDepartmentAndRole(userData.department.it, userData.role.admin)
+        userPage.slectDepartmentAndRole(userCardInfo.department.it, userCardInfo.role.admin)
     })
 
     it('create new user in IT department with sales role', function(){
-        userPage.slectDepartmentAndRole(userData.department.it, userData.role.sales)
+        userPage.slectDepartmentAndRole(userCardInfo.department.it, userCardInfo.role.sales)
     })
 
     it('create new user in sales department with admin role', function(){
-        userPage.slectDepartmentAndRole(userData.department.sales, userData.role.admin)
+        userPage.slectDepartmentAndRole(userCardInfo.department.sales, userCardInfo.role.admin)
     })
 
     it('create new user in accountants department with marketing role', function(){
-        userPage.slectDepartmentAndRole(userData.department.accountants, userData.role.marketing)
+        userPage.slectDepartmentAndRole(userCardInfo.department.accountants, userCardInfo.role.marketing)
     })
 
     it('create new user in marketing department with finance role', function(){
-        userPage.slectDepartmentAndRole(userData.department.marketing, userData.role.finance)
+        userPage.slectDepartmentAndRole(userCardInfo.department.marketing, userCardInfo.role.finance)
     })
 
     it('create new user in IT department with admin role', function(){
-        userPage.slectDepartmentAndRole(userData.department.it, userData.role.services)
+        userPage.slectDepartmentAndRole(userCardInfo.department.it, userCardInfo.role.services)
     })
 
     it('create new user in accountants department with admin role', function(){
-        userPage.slectDepartmentAndRole(userData.department.accountants, userData.role.admin)
+        userPage.slectDepartmentAndRole(userCardInfo.department.accountants, userCardInfo.role.admin)
     })
 
     afterEach(function(){
@@ -122,13 +134,22 @@ describe('negative/create users suite', function() {
 
     before(function() {
 
-        cy.fixture('create_user_data').then(function(usr) {userData = usr})
+        cy.fixture('create_user_data').then(function(usr) {userCardInfo = usr})
 
         cy.login(Cypress.env("USERNAME"), Cypress.env("PASSWD")).then(function(creds) {loginCredentials = creds})
 
     })
 
     beforeEach(function(){
+
+        let usrInfo = {
+            firstName     : faker.name.firstName(),
+            lastName      : faker.name.lastName(),
+            email         : userCardInfo.user.email,
+            contactNumber : faker.phone.phoneNumber(),
+            salesNumber   : faker.finance.account(),
+            title         : faker.name.title()
+        }
 
         cy.visit('./users', {
             onBeforeLoad (win) {
@@ -138,12 +159,12 @@ describe('negative/create users suite', function() {
 
         userPage.addUserBtn().should('have.text', 'Add User')
 
-        createUserCore({userEmail:userData.user.emailID})
+        createUserCore({usrInfo : usrInfo})
     })
 
     it('NEG/Create new user with existing email', function(){
-        userPage.departmentID().type(userData.department.it).type('{enter}')
-        userPage.roleID().type(userData.role.admin, {force: true}).type('{enter}')
+        userPage.departmentID().type(userCardInfo.department.it).type('{enter}')
+        userPage.roleID().type(userCardInfo.role.admin, {force: true}).type('{enter}')
     })
 
     afterEach(function(){
