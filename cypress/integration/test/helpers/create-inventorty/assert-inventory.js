80d58f74877c56daed06651c10742716be494c5a
EhabShaaban <ehabsfeki@gmail.com>
2021-05-23 17:26:50 +0200

    Add inventory asserts

--- cypress/integration/test/helpers/create-inventorty/assert-inventory.js
+++ cypress/integration/test/helpers/create-inventorty/assert-inventory.js
@@ -1,6 +1,8 @@
-import CreateInventoryPage from '../../../page/create-inventory-page'
+import InventoryPage from '../../../page/inventory-page'
+import Utils from '../../../utils/utils'
 
-const createInventory = new CreateInventoryPage()
+const inventoryPage = new InventoryPage()
+const utils = new Utils()
 
 /**
  * This should assert newly created inventory
@@ -9,58 +11,53 @@ const createInventory = new CreateInventoryPage()
  * 
  * accepting inventory object as an argument with the following values
  * 
- * @typedef AssertInventoryParams
-
- * @property {String} listingMileage
+ * @typedef AssertInventoryParams  
+ * generalInfo  
  * @property {String} cityFuelEco
- * @property {String} highwayFuelEco
- * @property {String} passengers
  * @property {String} combinedFuelEco
  * @property {String} engineDisplacement
+ * @property {String} highwayFuelEco
+ * @property {String} listingMileage
  * @property {String} listingPrice
- * @property {String} inventoryType
- * @property {String} fuelType
- * @property {String} gearType
- * @property {String} cylinders
- * @property {String} bodyType
- * @property {String} exteriorColor
- * @property {String} interiorColor
- * @property {String} numberOfDoors
- * @property {List}   features
+ * @property {String} passengers
+ * @property {String} vin
+ * purchaseInfo
+ * @property {String} purchaseComments
+ * @property {String} purchaseInvoice
+ * @property {String} purchaseMileage
+ * @property {String} purchasePrice
  * @property {String} source
  * @property {String} vendor
- * @property {String} purchasePrice
- * @property {String} purchaseMileage
- * @property {String} purchaseInvoice
- * @property {String} purchaseComments
  * 
- * @param {AssertInventory} listingMileage      - Inventory's listing mileage
- * @param {AssertInventory} cityFuelEco         - Inventory's city fuel economy
- * @param {AssertInventory} highwayFuelEco      - Inventory's highway fuel economy
- * @param {AssertInventory} passengers          - Inventory's passengers number
- * @param {AssertInventory} combinedFuelEco     - Inventory's combined economy value
- * @param {AssertInventory} engineDisplacement  - Inventory's engine displacement value
- * @param {AssertInventory} listingPrice        - Inventory's listing price
- * @param {AssertInventory} inventoryType       - Inventory's type
- * @param {AssertInventory} fuelType            - Inventory's fuel type
- * @param {AssertInventory} gearType            - Inventory's gear type
- * @param {AssertInventory} cylinders           - Inventory's cylinders
- * @param {AssertInventory} bodyType            - Inventory's body type
- * @param {AssertInventory} exteriorColor       - Inventory's exterior color
- * @param {AssertInventory} interiorColor       - Inventory's interior color
- * @param {AssertInventory} numberOfDoors       - Inventory's number of doors
- * @param {AssertInventory} features            - Inventory's list of features
- * @param {AssertInventory} source              - Inventory's source
- * @param {AssertInventory} vendor              - Inventory's vendor
- * @param {AssertInventory} purchasePrice       - Inventory's purchase price
- * @param {AssertInventory} purchaseMileage     - Inventory's purchase mileage
- * @param {AssertInventory} purchaseInvoice     - Inventory's purchase invoice
- * @param {AssertInventory} purchaseComments    - Inventory's purchase comments
+ * @param {AssertInventory} cityFuelEco           - Inventory's city fuel economy
+ * @param {AssertInventory} combinedFuelEco       - Inventory's combined economy value
+ * @param {AssertInventory} engineDisplacement    - Inventory's engine displacement value
+ * @param {AssertInventory} highwayFuelEco        - Inventory's highway fuel economy
+ * @param {AssertInventory} listingMileage        - Inventory's listing mileage
+ * @param {AssertInventory} listingPrice          - Inventory's listing price
+ * @param {AssertInventory} passengers            - Inventory's passengers number
+ * @param {AssertInventory} vin                   - Inventory's vin hash code
+ * @param {AssertInventory} purchaseComments      - Inventory's purchase comments
+ * @param {AssertInventory} purchaseInvoice       - Inventory's purchase invoice
+ * @param {AssertInventory} purchaseMileage       - Inventory's purchase mileage
+ * @param {AssertInventory} purchasePrice         - Inventory's purchase price
+ * @param {AssertInventory} source                - Inventory's source
+ * @param {AssertInventory} vendor                - Inventory's vendor
  */
 module.exports =({
     inventory
 }) => {
 
     console.log(inventory)
+
+    inventoryPage.inventoryStatusId().should('have.text', 'In Stock')
+    inventoryPage.vinId().should('have.text', inventory.generalInfo.vin)
+    inventoryPage.bodyTypeId().should('contain', inventory.inventoryCore.bodyType)
+    inventoryPage.mileageId().should('contain', utils.addThousandSeparator(inventory.generalInfo.listingMileage))
+
+    inventoryPage.listingPriceId().should('contain', utils.addThousandSeparator(inventory.generalInfo.listingPrice))
+    inventoryPage.purchasePrice().should('contain', utils.addThousandSeparator(inventory.purchaseInfo.purchasePrice))
+    
+    inventoryPage.gearTypeId().should('contain', inventory.inventoryCore.gearType.charAt(0).toUpperCase()+inventory.inventoryCore.gearType.slice(1))
 
 }
