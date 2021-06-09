e938a0207531d0830c1706fce4a5ce8492bf5878
EhabShaaban <ehabsfeki@gmail.com>
2021-06-09 20:28:20 +0200

    Add create bos helper

---
+++ cypress/integration/test/helpers/create-bos/create-cash-bos.js
@@ -0,0 +1,49 @@
+import CreateBosPage from "../../../page/create-bos-page"
+import InventoryPage from "../../../page/inventory-page"
+import Utils from "../../../utils/utils"
+
+const utils = new Utils()
+
+/**
+ * This should create bos for an inventory
+ * 
+ * impacted page  >> create-bos-page
+ * impacted route >> inventory/create-bill-of-sell/stock_number/vin_code
+ */
+module.exports = ({
+    inventory
+}) => {
+
+    const inventoryPage = new InventoryPage()
+    const createBosPage = new CreateBosPage()
+    let hstPrice = (inventory.listingPrice)*0.13
+
+    inventoryPage.createBillOfSale()
+
+    createBosPage.cashOptionId().click()
+
+    createBosPage.distanceTravelledId().type("45")
+
+    createBosPage.purchaseInformationId().click()
+
+    createBosPage.findCustomerId().click()
+
+    createBosPage.firstCustomerInput().click()
+
+    createBosPage.saveCustomerSpan().click()
+
+    createBosPage.clickTermOfSettlement()
+
+    createBosPage.vinId().should('have.text', inventory.vin)
+
+    createBosPage.hstOnSubTotalInput().should('contain.text', utils.addThousandSeparator((hstPrice).toFixed(2)))
+
+    createBosPage.subTotalId().should('contain.text', "$"+utils.addThousandSeparator((inventory.listingPrice).toFixed(2)))
+
+    createBosPage.subTotalWithHst().should('contain.text', "$"+utils.addThousandSeparator(((inventory.listingPrice)+hstPrice).toFixed(2)))
+
+    createBosPage.saveBos().should('have.text', 'Save').click();
+
+    cy.wait(10000)
+    
+}
