5c8069cb4c3667b55ca81a628f4678c39aaaa3b2
EhabShaaban <ehabsfeki@gmail.com>
2021-06-09 20:26:49 +0200

    Add inventory getter & Route after bos creation

--- cypress/integration/test/create-bos/create-bos-whole-sale-tests.spec.js
+++ cypress/integration/test/create-bos/create-bos-whole-sale-tests.spec.js
@@ -1,11 +1,14 @@
 /// <reference types="Cypress" />
 
 import {adaptToReduxPersist} from '../../utils/redux'
+import createCashBos from '../../test/helpers/create-bos/create-cash-bos'
+import {getInventory} from '/home/ehab/dev/lentrak/ui-framework/cypress/support/commands.js'
 
 let loginCredentials
 let stockNumber
+let inventory = getInventory()
 
-describe('create bos suite', function() {
+describe('create cash bos suite', function() {
 
     before(function() {
 
@@ -25,6 +28,9 @@ describe('create bos suite', function() {
                     win.localStorage.setItem('persist:root', adaptToReduxPersist(loginCredentials))
                 }
             })
+            createCashBos({inventory:inventory})
+            console.log(inventory)
+            cy.visit('./inventory/'+stockNumber)
         })
     })
 
